57d80bcb28bba8634a999a31af8fe44303f67a1e
mohan <sahumohan338@gmail.com>
2022-07-27 04:56:48 -0700

    [mohan]add: Flip coin problem

---
+++ BasicLogical/BasicLogical/Logic.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BasicLogical
+{
+    public class Logic
+    {
+        public static void Calculate()
+        {
+            int tailCount = 0;
+            int headCount = 0;
+
+            double tailPercentage = 0;
+            double headPercentage = 0;
+
+            Console.WriteLine("Enter number of Flips: ");
+            int Flips = Convert.ToInt32(Console.ReadLine());
+
+            for (int i = 0; i < Flips; i++)
+            {
+                Random random = new Random();
+                int randValue = random.Next(2);
+                Console.WriteLine("Random values: " + randValue);
+                if (randValue == 0)
+                {
+                    Console.WriteLine("Tail");
+                    tailCount++;
+                }
+                else if (randValue == 1)
+                {
+                    Console.WriteLine("Head");
+                    headCount++;
+                }
+                else
+                {
+                    Console.WriteLine("No flips");
+                }
+            }
+            headPercentage = (headCount * 100) / Flips;
+            tailPercentage = (tailCount * 100) / Flips;
+
+            Console.WriteLine("Percentage of head: " + headPercentage);
+            Console.WriteLine("Percentage of tail: " + tailPercentage);
+
+        }
+    }
+}
--- BasicLogical/BasicLogical/Program.cs
+++ BasicLogical/BasicLogical/Program.cs
@@ -6,24 +6,7 @@ namespace BasicLogical
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("enter first name: ");
-            string username = Console.ReadLine();
-            int num = username.Length;
-            if (num >= 3 && num <= 5)
-            {
-                Console.WriteLine("Hello " + username + ", How are you");
-            }
-            else if (num > 5)
-            {
-                Console.WriteLine("the username should be greater then 5 char");
-            }
-            else
-            {
-                Console.WriteLine("username should be greater than 3 char");
-            }
-
-
-
+            Logic.Calculate();
         }
     }
 }
