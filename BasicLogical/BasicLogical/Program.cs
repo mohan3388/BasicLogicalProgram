6f9bb2988adb0c8d2cefed60e267a98c710f1f27
mohan <sahumohan338@gmail.com>
2022-07-27 02:55:31 -0700

    [mohan]add: hello world program

---
+++ BasicLogical/BasicLogical/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+
+namespace BasicLogical
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("enter first name: ");
+            string username = Console.ReadLine();
+            int num = username.Length;
+            if (num >= 3 && num <= 5)
+            {
+                Console.WriteLine("Hello " + username + ", How are you");
+            }
+            else if (num > 5)
+            {
+                Console.WriteLine("the username should be greater then 5 char");
+            }
+            else
+            {
+                Console.WriteLine("username should be greater than 3 char");
+            }
+
+
+
+        }
+    }
+}
