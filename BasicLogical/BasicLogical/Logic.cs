e442984b524ba882c99ba7ba3e5b2c8b3e636463
Mohan Sahu <68227271+mohan3388@users.noreply.github.com>
2022-07-27 05:35:58 -0700

    Update Logic.cs

--- BasicLogical/BasicLogical/Logic.cs
+++ BasicLogical/BasicLogical/Logic.cs
@@ -10,15 +10,13 @@ namespace BasicLogical
     {
         public static void Calculate()
         {
-            Console.WriteLine("Enter number: ");
+           Console.WriteLine("Enter number");
             int num = Convert.ToInt32(Console.ReadLine());
 
-            for (int i = 0; i <= num; i++)
+            for(int i = 1; i <= num; i++)
             {
-                int power = (int)Math.Pow(2, i);
-                Console.WriteLine("2" + " ^ " + i + " = " + power);
+                Console.WriteLine("1"+"/"+i+"+");
             }
-
         }
     }
 }
