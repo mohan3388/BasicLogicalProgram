d1fa891c42edb895944499cf7a1f69898f49fa0d
mohan <sahumohan338@gmail.com>
2022-07-29 23:59:14 -0700

    [mohan]add: Quotient and Reminder

--- BasicLogical/BasicLogical/Logic.cs
+++ BasicLogical/BasicLogical/Logic.cs
@@ -10,24 +10,14 @@ namespace BasicLogical
     {
         public static void Calculate()
         {
-            int count = 0;
-            Console.WriteLine("Enter a number: ");
+            Console.WriteLine("Enter number: ");
             int num = Convert.ToInt32(Console.ReadLine());
-            for (int i = 2; i <= num; i++)
-            {
-               for(int j=1; j<=i; j++)
-                {
-                    if(i%j==0)
-                    {
-                        count++;
-                    }
-                }
-                if(count==2)
-                {
-                    Console.WriteLine(i);
-                }
-                count = 0;
-            }
+
+            int Quotient = num / 10;
+            int Reminder = num % 10;
+
+            Console.WriteLine("Quotient is: "+Quotient);
+            Console.WriteLine("Reminder is: "+Reminder);
         }
     }
 }
