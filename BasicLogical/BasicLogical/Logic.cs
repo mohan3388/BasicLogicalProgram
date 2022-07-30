8c3852c1703a5ae7f8bc14729905ec87fc923c12
mohan <sahumohan338@gmail.com>
2022-07-30 00:25:34 -0700

    [mohan]add: Even add Program

--- BasicLogical/BasicLogical/Logic.cs
+++ BasicLogical/BasicLogical/Logic.cs
@@ -10,21 +10,18 @@ namespace BasicLogical
     {
         public static void Calculate()
         {
-           
-            Console.WriteLine("Enter number: ");
-            int num1 = Convert.ToInt32(Console.ReadLine());
-            int num2 = Convert.ToInt32(Console.ReadLine());
-            Console.WriteLine("Before Swapping two numbers");
-            Console.WriteLine("value of num1: "+num1);
-            Console.WriteLine("value of num2: "+num2);
-
-            num1 = num1 * num2;
-            num2 = num1 / num2;
-            num1 = num1 / num2;
-
-            Console.WriteLine("After Swapping two number: ");
-            Console.WriteLine("value of num1: " + num1);
-            Console.WriteLine("value of num2: " + num2);
+            Console.WriteLine("Even Odd Program");
+            Console.WriteLine("Enter number:");
+            int num = Convert.ToInt32(Console.ReadLine());
+            if(num%2==0)
+            {
+                Console.WriteLine(num+" is even number");
+            }
+            else
+            {
+                Console.WriteLine(num + " is odd number");
+            }
+            
         }
     }
 }
